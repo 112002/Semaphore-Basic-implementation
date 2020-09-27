6c59b3b13c468dd780521943224005d193b17b63
NUPOOR RAJ <59130972+112002@users.noreply.github.com>
2020-09-27 13:30:05 -0700

    Create semaphore.c

---
+++ semaphore.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define SEM_MUTEX_NAME "/sem-mutex"
+#define SEM_BUFFER_COUNT_NAME "/sem-buffer-count"
+#define SEM_SPOOL_SIGNAL_NAME "/sem-spool-signal"
